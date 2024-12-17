e744f4dca4a6b57bf1f89f107ff9fa9f92f8fb80
Seven Du <shiweidu@outlook.com>
2024-12-17 23:08:46 +0800

    chore: WIP

--- lib/src/effect_scope.dart
+++ lib/src/effect_scope.dart
@@ -1,4 +1,56 @@
+import 'effect.dart';
+import 'system.dart';
+import 'types.dart';
+
 EffectScope? activeEffectScope;
 int activeScopeTrackId = 0;
 
-class EffectScope {}
+class EffectScope implements Subscriber, Notifiable {
+  @override
+  Link? deps;
+
+  @override
+  Link? depsTail;
+
+  @override
+  SubscriberFlags flags = SubscriberFlags.none;
+
+  @override
+  Notifiable? nextNotify;
+
+  int trackId = nextTrackId();
+
+  @override
+  void notify() {
+    if ((flags & SubscriberFlags.runInnerEffects) != 0) {
+      flags &= ~SubscriberFlags.runInnerEffects;
+      Link? link = deps;
+      do {
+        final dep = link!.dep;
+        if (dep is Notifiable) {
+          (dep as Notifiable).notify();
+        }
+
+        link = link.nextDep;
+      } while (link != null);
+    }
+  }
+
+  T run<T>(T Function() fn) {
+    final prevSub = activeEffectScope;
+    final prevTrackId = activeScopeTrackId;
+    activeEffectScope = this;
+    activeScopeTrackId = trackId;
+    try {
+      return fn();
+    } finally {
+      activeEffectScope = prevSub;
+      activeScopeTrackId = prevTrackId;
+    }
+  }
+
+  void stop() {
+    startTrack(this);
+    endTrack(this);
+  }
+}

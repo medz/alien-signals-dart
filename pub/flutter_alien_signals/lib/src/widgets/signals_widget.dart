766e02fbf6721e9d009ee0750fbd0ab0f8c37f93
Seven Du <5564821+medz@users.noreply.github.com>
2025-07-03 14:08:03 +0800

    chore: only alien_signals

--- pub/flutter_alien_signals/lib/src/widgets/signals_widget.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/widgets.dart';
-
-import '../mixins/signals.dart';
-
-abstract class SignalsWidget extends StatelessWidget with Signals {
-  const SignalsWidget({super.key});
-}

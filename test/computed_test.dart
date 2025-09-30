23e6b4ef4955560df29d93e97287b8ab0cf17d2b
Seven Du <5564821+medz@users.noreply.github.com>
2025-10-01 00:49:03 +0800

    Update computed tests to use new signal access syntax

--- test/computed_test.dart
+++ test/computed_test.dart
@@ -4,46 +4,46 @@ import 'package:test/test.dart';
 void main() {
   test("should correctly propagate changes through computed signals", () {
     final s = signal(0);
-    final c1 = computed((_) => s.value % 2);
-    final c2 = computed((_) => c1.value);
-    final c3 = computed((_) => c2.value);
+    final c1 = computed((_) => s() % 2);
+    final c2 = computed((_) => c1());
+    final c3 = computed((_) => c2());
 
-    c3.value;
-    s.value = 1;
-    c2.value;
-    s.value = 3;
+    c3();
+    s(1);
+    c2();
+    s(3);
 
-    expect(c3.value, 1);
+    expect(c3(), 1);
   });
 
   test("should propagate updated source value through chained computations",
       () {
     final s = signal(0);
-    final a = computed((_) => s.value);
-    final b = computed((_) => a.value % 2);
-    final c = computed((_) => s.value);
-    final d = computed((_) => b.value + c.value);
+    final a = computed((_) => s());
+    final b = computed((_) => a() % 2);
+    final c = computed((_) => s());
+    final d = computed((_) => b() + c());
 
-    expect(d.value, 0);
-    s.value = 2;
-    expect(d.value, 2);
+    expect(d(), 0);
+    s(2);
+    expect(d(), 2);
   });
 
   test("should handle flags are indirectly updated during checkDirty", () {
     final a = signal(false);
-    final b = computed((_) => a.value);
+    final b = computed((_) => a());
     final c = computed((_) {
-      b.value;
+      b();
       return 0;
     });
     final d = computed((_) {
-      c.value;
-      return b.value;
+      c();
+      return b();
     });
 
-    expect(d.value, false);
-    a.value = true;
-    expect(d.value, true);
+    expect(d(), false);
+    a(true);
+    expect(d(), true);
   });
 
   test("should not update if the signal value is reverted", () {
@@ -51,14 +51,14 @@ void main() {
     final s = signal(0);
     final c = computed((_) {
       times++;
-      return s.value;
+      return s();
     });
 
-    c.value;
+    c();
     expect(times, 1);
-    s.value = 1;
-    s.value = 0;
-    c.value;
+    s(1);
+    s(0);
+    c();
     expect(times, 1);
   });
 }
